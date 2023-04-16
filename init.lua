012e43c1f93423dc9d166110cc923ed2b458752c
DarckRM <412543224@qq.com>
2023-04-16 12:02:40 +0800

    init commit

---
+++ init.lua
@@ -0,0 +1,10 @@
+require("plugins.plugins-setup")
+
+require("core.options")
+require("core.keymaps")
+
+require("plugins.lualine")
+require("plugins.nvim-tree")
+require("plugins/lsp")
+require("plugins/cmp")
+-- require("plugins.treesitter")
